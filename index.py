73559c69302c7cec48991080efa6aa428b7fd21a
Tanvir <duggaltanvir@gmail.com>
2019-08-22 15:25:39 -0400

    2.0 Gett9ing the tweets
    The step to get tweets and trying to like them

--- index.py
+++ index.py
@@ -19,9 +19,10 @@ class TwitterBot():
         self.bot      = webdriver.Chrome()
     
     def login(self):
-        print("test")
+#        print("test")
         bot = self.bot
         bot.get("https://twitter.com/")
+        bot.maximize_window()
         time.sleep(6)
         email    = bot.find_element_by_name('session[username_or_email]')
         password = bot.find_element_by_name('session[password]')
@@ -34,11 +35,20 @@ class TwitterBot():
         
     def like_tweet(self, hastag):
         bot = self.bot
-        bot.get('')
+        bot.get('https://twitter.com/search?q=' + hastag + '&src=typeahead_click')
+        time.sleep(5)
+        for i in range(1, 2):
+            bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+            time.sleep(2)
+            tweets = bot.find_element_by_class_name('css-1dbjc4n')
+#            links = [elem.get_attribute('data-permalink-path') for elem in tweets]
+            print(tweets.get_property('attributes')[0])
+        
     
 def Main():
     ed = TwitterBot("duggaltanvir@gmail.com", "Coolmaster@008")
     ed.login()
+    ed.like_tweet("webdevelopment")
     
 if __name__ == '__main__':
     Main()
